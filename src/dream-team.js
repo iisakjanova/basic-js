8b60d229b50cd5b02e5f9ae35daed092ce41a580
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-04-15 21:26:41 -0500

    feat: solve count-cats, carbon-dating, dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(members instanceof Array === false) {
+    return false;
+  }
+
+  let teamNameArray = [];
+
+  for(let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      const name = members[i].trimStart();
+      teamNameArray.push(name[0].toUpperCase());
+    }
+  }
+
+  if(teamNameArray.length === 0) {
+    return false;
+  } 
+
+  return teamNameArray.sort().join('');
 }
 
 module.exports = {
