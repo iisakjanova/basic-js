7532bbdfec7b3079af6e7661a7ca474627771fea
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-04-17 11:51:26 -0500

    feat: solve transform-array and common-character-count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let hash = {};
+  let counter = 0;
+
+  for (let i = 0; i < s1.length; i++) {
+    if(hash[s1[i]]) {
+      hash[s1[i]]++;
+    } else {
+      hash[s1[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < s2.length; i++) {
+    if(hash[s2[i]] > 1) {
+      counter++;
+      hash[s2[i]]--;
+    } else if(hash[s2[i]] === 1) {
+      counter++;
+      delete hash[s2[i]];
+    }
+  }
+
+  return counter;
 }
 
 module.exports = {
