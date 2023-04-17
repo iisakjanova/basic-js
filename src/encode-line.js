f02556e9a79c6a0455a7d5f58b435cbc2f7d1789
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-04-17 18:09:55 -0500

    feat: solve dns-stats, encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let encodedStr = '';
+  let curChar = '';
+  let counter = 0;
+
+  for (let i = 0; i <= str.length; i++) {
+    if (i === 0) {
+      curChar = str[i];
+      counter++
+    } else if (str[i] === curChar) {
+      counter++;
+    } else {
+        if(counter > 1) {
+          encodedStr = encodedStr + counter.toString() + curChar;
+        } else {
+          encodedStr = encodedStr + curChar;
+        }
+      
+        curChar = str[i];
+        counter = 1;
+    } 
+  }
+
+  return encodedStr;
 }
 
 module.exports = {
